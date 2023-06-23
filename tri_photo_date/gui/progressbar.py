adc9d1fbb009933218442e3e5093f85985b1fc33
Daguhh <code.daguhh@zaclys.net>
2023-06-23 12:38:16 +0200

    update progressbar

--- tri_photo_date/gui/progressbar.py
+++ tri_photo_date/gui/progressbar.py
@@ -8,19 +8,17 @@ PROG_NORMAL = 1
 
 try:
     import pyqtgraph as pg
+    from tri_photo_date.gui.speedprogressbar import SpeedProgressBar
     progress_bar_mode = PROG_SPEED_BAR
 except ModuleNotFoundError:
     progress_bar_mode = PROG_NORMAL
 
 if progress_bar_mode == PROG_SPEED_BAR:
 
-    class MyProgressBar(pg.PlotWidget):
+    class MyProgressBar(SpeedProgressBar):
 
-        def __init__(self, parent=None):
-
-            super().__init__(parent, background='w')
-
-            self.parent = parent
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
 
             self.label = self.add_label()
 
@@ -29,66 +27,15 @@ if progress_bar_mode == PROG_SPEED_BAR:
             fake_layout.addWidget(self)
             self.prev_layout = fake_layout
 
-            self.plotItem.showAxes(False)
-            self.plotItem.showGrid(False)
-
-            self.bargraph = None
+        def update(self, v, text="", text2=""):
+            super().update(v)
+            QApplication.processEvents()  # keep the GUI responsive
+            self.text_label.setText(text)
 
         def add_label(self):
 
             self.text_label = QLabel("")
             return self.text_label
-
-        def init(self, n):
-
-            n = n + 1
-
-            self.chunk_nb = min(500, n) # max bar number
-            self.chunk_width = n // self.chunk_nb # "width" of bar
-            self.chunk_index = 0 # count bar index
-            self.chunk_last_index = 0 # hold previous bar index
-
-            del(self.bargraph)
-            self.clear()
-
-            x = range(n // self.chunk_width + 1)
-            y = [0 for _ in x]
-
-            self.bargraph = pg.BarGraphItem(x = x, height = y, width = 1, brush ='c')
-            self.addItem(self.bargraph)
-
-            # start a timer to update the plot
-            self.timer = pg.QtCore.QTimer()
-            self.timer.timeout.connect(self.update_plot)
-            self.timer.start(100)  # 50ms
-
-            self.tic = time.time()
-
-            self.new_ys = self.bargraph.opts['height']
-
-        def update_plot(self):
-
-            self.bargraph.setOpts(**{'heigth':self.new_ys})
-
-        def update(self, x, text="", text2=""):
-
-            self.text_label.setText(text)
-            QApplication.processEvents()  # keep the GUI responsive
-
-            # Create a new bar on graph
-            self.chunk_index = x // self.chunk_width
-            if self.chunk_index != self.chunk_last_index:
-                self.chunk_last_index = self.chunk_index
-
-                # get last elapsed time
-                toc = time.time() - self.tic
-                self.tic = time.time()
-
-                # Set speed at chunk_index
-                speed = (self.chunk_width) / toc # speed of chunk
-                ys = self.bargraph.opts['height']
-                ys[self.chunk_index] = speed
-                self.new_ys = ys
 
         def move_to_layout(self, new_layout):
 
---
+++ tri_photo_date/gui/speedprogressbar.py
@@ -0,0 +1,130 @@
+from collections import deque
+import pyqtgraph as pg
+import time
+
+class SpeedProgressBar(pg.PlotWidget):
+    """A ProgressBar that show both progress and speed:
+        - abscissa : progress [unit], with unit, the unit of data given by user
+        - ordonnate : speed [unit/time]
+    """
+
+    def __init__(self, parent=None, nb_sections=500, smooth_window=4):
+        """Initialize the plot : Hide axis, prevent mouse interactions
+        initialize values
+
+        Parameters
+        ----------
+        nb_sections : int
+            Number of vertical bar in the graph
+        smooth_window : int
+            Use average of last speed bars value to smooth datas (prettier?)
+        """
+
+        super().__init__(parent, background='w')
+
+        # Number of bar in the graph
+        self.nb_sections = nb_sections
+
+        # Hide axis
+        self.plotItem.showAxes(False)
+        self.plotItem.showGrid(False)
+
+        # Store last speed values to smooth_window the graph
+        self.speeds = deque([10**-15], maxlen=smooth_window)
+
+        # Disable mouse interactions
+        self.setMouseEnabled(False)
+        self.plotItem.setMenuEnabled(False)
+        self.plotItem.setMouseEnabled(False)
+        self.wheelEvent = lambda *args, **kwargs: None
+        self.mouseDragEvent = lambda *args, **kwargs: None
+        self.mousePressEvent = lambda *args, **kwargs: None
+        self.mouseReleaseEvent = lambda *args, **kwargs: None
+
+        # Store the graph object
+        self.bargraph = None
+
+    def init(self, max_value):
+        """Initialise a bar graph at zero for each x
+
+        Parameters
+        ----------
+        max_value : int, float
+            value at which progressbar reach 100%, could be number of items, sum of files size to process...
+            Set to 100 to work in percentage
+        """
+
+        self.nb_sections = int(min(self.nb_sections, max_value)) # max bar number
+        self.bar_width = max_value / self.nb_sections # "width" of bar
+        self.bar_index = 0 # count bar index
+        self._bar_previous_index = 0 # hold previous bar index
+
+        # remove last bar and clean plot
+        del(self.bargraph)
+        self.clear()
+
+        # Init bar at 0
+        x = range(self.nb_sections + 1)
+        y = [0 for _ in x]
+        self.bargraph = pg.BarGraphItem(x = x, height = y, width = 1, brush ='c', pen=pg.mkPen(None))
+        self.addItem(self.bargraph)
+
+        # start a timer to update the plot
+        self.timer = pg.QtCore.QTimer()
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start(100)  # 50ms
+
+        # Start to mesure time
+        self.tic = None# time.time()
+
+        # Store bargraph values
+        self.new_ys = self.bargraph.opts['height']
+
+    def update_plot(self):
+        """Update bar graph given new Y values when called by timer"""
+
+        self.bargraph.setOpts(**{'heigth':self.new_ys})
+
+    def update(self, progress_value):
+        """update bar Y values
+
+        Parameters
+        ----------
+        progress_value : int, float
+            progress_value / max_value = percentage
+        """
+
+        # Create a new bar on graph
+        bar_index = int(progress_value // self.bar_width)
+
+        # while bar is not complete skip
+        if bar_index == self._bar_previous_index:
+            return
+
+        # get last elapsed time
+        toc = time.time() - self.tic if self.tic else 3600 # first time init
+        self.tic = time.time()
+
+        # (let's say we loop over 1 Go (max_value) of files, and all are 1Ko
+        # except 1 that is 500Mo (progress_value),
+        # the last will progress of multiples row (nb_bars) in one time)
+        nb_bars = bar_index -self._bar_previous_index
+
+        # compute speed
+        new_speed = nb_bars * self.bar_width / toc # speed of bar
+
+        # if new progress_value is larger than bar width
+        for index in range(self._bar_previous_index + 1, bar_index + 1):
+
+            self.speeds.append(new_speed)
+
+            # Set mean speed at bar_index
+            ys = self.bargraph.opts['height']
+            ys[index] = sum(self.speeds) / len(self.speeds)
+
+        self.new_ys = ys
+
+        self.speeds.append(new_speed)
+        self._bar_previous_index = bar_index
+
+
